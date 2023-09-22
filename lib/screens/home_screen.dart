3c704e46c0335a2590f22216f4983b420f947de9
Mohammad Javad Javadi <mohammadjavad.javadir@gmail.com>
2023-09-22 17:15:30 +0330

    Create Project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.grey[900],
+      ),
+      backgroundColor: Colors.grey[900],
+    );
+  }
+}
